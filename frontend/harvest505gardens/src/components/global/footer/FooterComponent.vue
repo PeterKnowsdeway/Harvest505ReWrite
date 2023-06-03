4ebe0055517a9ad406f90d694beff32c8dd11d04
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2023-06-03 07:16:46 -0500

    Updated footer component

--- frontend/harvest505gardens/src/components/global/footer/FooterComponent.vue
+++ frontend/harvest505gardens/src/components/global/footer/FooterComponent.vue
@@ -1,170 +1,135 @@
 <template>
     <!-- Footer -->
     <footer class="text-center text-lg-start bg-light text-muted custom-container">
-      <!-- Section: Links  -->
-      <section class="">
-        <div class="text-center text-md-start mt-5">
-          <!-- Grid row -->
-          <div class="row mt-3">
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <!-- Content -->
-              <router-link to="/" class="navbar-brand">
-                <img src="@/assets/logo3.svg" alt="Farm Logo" class="logo-img" />
-              </router-link>
-              <p>
-                Harvest505Gardens LLC: Cultivating Nature's Bounty for a Sustainable Future
-              </p>
-            </div>
+        <!-- Section: Links  -->
+        <section class="">
+            <div class="text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <router-link to="/" class="navbar-brand">
+                            <img src="@/assets/logo3.svg" alt="Farm Logo" class="logo-footer" />
+                        </router-link>
+                        <p class="footer-p">
+                            Harvest505Gardens LLC: Cultivating Nature's Bounty for a Sustainable Future
+                        </p>
+                    </div>
 
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <!-- Links -->
-                <h6 class="text-uppercase fw-bold mb-4">
-                  Useful links
-                </h6>
-                <p>
-                  <a href="#!" class="text-reset">Pricing</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-reset">Settings</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-reset">Orders</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-reset">Help</a>
-                </p>
-              </div>
-              <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold mb-4">
-                Useful links
-              </h6>
-              <p>
-                <a href="#!" class="text-reset">Pricing</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Settings</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Orders</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Help</a>
-              </p>
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Products
+                        </h6>
+                        <p>
+                            <a href="#!" class="link-styles">General</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="link-styles">Fruits</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="link-styles">Vegtables</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Quick Links
+                        </h6>
+                        <p>
+                            <a href="#!" class="link-styles">Contact</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="link-styles">About</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="link-styles">Blog</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                        <p>
+                            <font-awesome-icon icon="fa-solids fa-home" class="me-3" />
+                            <a href="https://www.google.com/maps/search/?api=1&query=505%20Hwy%20BB%2C%20Fair%20Play%2C%20Missouri%2C%20MO%2065649" target="_blank" class="link-styles">505 Hwy BB, Fair Play, Missouri, MO 65649</a>
+                        </p>
+                        <p>
+                            <font-awesome-icon icon="fa-solids fa-envelope" class="me-3" />
+                            <a href="mailto:sharonjwelter@gmail.com" class="link-styles">sharonjwelter@gmail.com</a>
+                        </p>
+                        <p>
+                            <font-awesome-icon icon="fa-solids fa-phone" class="me-3" />
+                            <a href="tel:4176545036" class="link-styles">417-654-5036</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
             </div>
-            <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-              <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-              <p>
-                <i class="fas fa-envelope me-3"></i>
-                info@example.com
-              </p>
-              <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-              <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </section>
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-center align-items-center p-4">
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block">
+                <span class="footer-p">Get connected with us on social networks:</span>
             </div>
-            <!-- Grid column -->
-          </div>
-          <!-- Grid row -->
+            <!-- Left -->
+
+            <!-- Right -->
+            <div class="d-flex">
+                <!-- Section: Social media -->
+                <section>
+                    <!-- Facebook -->
+                    <a class="btn text-white btn-floating m-1 expand-icon" style="background-color: #3b5998;" href="#!" role="button"
+                        aria-label="Find out more about us on Facebook"><font-awesome-icon
+                            icon="fa-brands fa-facebook-square" />
+                    </a>
+
+                    <!-- Twitter -->
+                    <a class="btn text-white btn-floating m-1 expand-icon" style="background-color: #55acee;" href="#!" role="button"
+                        aria-label="Find out more about us on Twitter"><font-awesome-icon icon="fa-brands fa-twitter" />
+                    </a>
+
+                    <!-- Google -->
+                    <a class="btn text-white btn-floating m-1 expand-icon" style="background-color: #dd4b39;" href="#!" role="button"
+                        aria-label="Find out more about us on Google"><font-awesome-icon icon="fa-brands fa-google" />
+                    </a>
+
+                    <!-- Instagram -->
+                    <a class="btn text-white btn-floating m-1 expand-icon" style="background-color: #ac2bac;" href="#!" role="button"
+                        aria-label="Find out more about us on Instagram"><font-awesome-icon icon="fa-brands fa-instagram" />
+                    </a>
+
+                    <!-- GitHub -->
+                    <a class="btn text-white btn-floating m-1 expand-icon" style="background-color: #333333;" href="#!" role="button"
+                        aria-label="Find out more about us on GitHub"> <font-awesome-icon icon="fa-brands fa-github" />
+                    </a>
+                </section>
+                <!-- Section: Social media -->
+            </div>
+            <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+        <hr class="divider">
+        <!-- Terms of Service and Privacy Policy -->
+        <div class="text-center p-4" aria-label="Terms of Service and Privacy Policy">
+            <p class="footer-p">
+                &copy; 2023 Harvest505Gardens LLC. All rights reserved.
+                &nbsp;&nbsp;&nbsp;
+                <a href="#" class="link-styles">Terms of Service</a>
+                &nbsp;&nbsp;&nbsp;
+                <a href="#" class="link-styles">Privacy Policy</a>
+            </p>
         </div>
-      </section>
-      <!-- Section: Social media -->
-      <section class="d-flex justify-content-center p-4">
-        <!-- Left -->
-        <div class="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
-        </div>
-        <!-- Left -->
-  
-        <!-- Right -->
-        <div>
-          <!-- Section: Social media -->
-          <section class="mb-4">
-            <!-- Facebook -->
-            <a
-              class="btn text-white btn-floating m-1"
-              style="background-color: #3b5998;"
-              href="#!"
-              role="button"
-              aria-label="Find out more about us on Facebook"
-            ><i class="fab fa-facebook-f"></i
-            ></a>
-  
-            <!-- Twitter -->
-            <a
-              class="btn text-white btn-floating m-1"
-              style="background-color: #55acee;"
-              href="#!"
-              role="button"
-              aria-label="Find out more about us on Twitter"
-            ><i class="fab fa-twitter"></i
-            ></a>
-  
-            <!-- Google -->
-            <a
-              class="btn text-white btn-floating m-1"
-              style="background-color: #dd4b39;"
-              href="#!"
-              role="button"
-              aria-label="Find out more about us on Google"
-            ><i class="fab fa-google"></i
-            ></a>
-  
-            <!-- Instagram -->
-            <a
-              class="btn text-white btn-floating m-1"
-              style="background-color: #ac2bac;"
-              href="#!"
-              role="button"
-              aria-label="Find out more about us on Instagram"
-            ><i class="fab fa-instagram"></i
-            ></a>
-  
-            <!-- LinkedIn -->
-            <a
-              class="btn text-white btn-floating m-1"
-              style="background-color: #0082ca;"
-              href="#!"
-              role="button"
-              aria-label="Find out more about us on LinkedIn"
-            ><i class="fab fa-linkedin-in"></i
-            ></a>
-  
-            <!-- GitHub -->
-            <a
-              class="btn text-white btn-floating m-1"
-              style="background-color: #333333;"
-              href="#!"
-              role="button"
-              aria-label="Find out more about us on GitHub"
-            ><i class="fab fa-github"></i
-            ></a>
-          </section>
-          <!-- Section: Social media -->
-        </div>
-        <!-- Right -->
-      </section>
-      <!-- Section: Social media -->
-      <hr class="divider">
-      <!-- Terms of Service and Privacy Policy -->
-      <div class="text-center p-4" aria-label="Terms of Service and Privacy Policy">
-        <p>
-          &copy; 2023 Harvest505Gardens LLC. All rights reserved.
-          &nbsp;&nbsp;&nbsp;
-          <a href="#" class="policy-links">Terms of Service</a>
-          &nbsp;&nbsp;&nbsp;
-          <a href="#" class="policy-links">Privacy Policy</a>
-        </p>
-      </div>
-      <!-- Terms of Service and Privacy Policy -->
+        <!-- Terms of Service and Privacy Policy -->
     </footer>
     <!-- Footer -->
 </template>
--- frontend/harvest505gardens/src/main.js
+++ frontend/harvest505gardens/src/main.js
@@ -2,6 +2,29 @@ import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+
+/* import specific icons */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faUserSecret, faTwitter, faFacebookSquare, faInstagram, faGoogle, faLinkedinIn, faGithub, faHome, faEnvelope, faPhone )
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js"
